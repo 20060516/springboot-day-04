17df853275b339056986bc5abae7ae3988982ec3
20060516 <pavithra.p2023ece@sece.ac.in>
2025-07-07 09:29:59 +0530

    Merged remote changes

---
+++ src/main/java/com/example/springboot/models/Employee.java
@@ -0,0 +1,41 @@
+package com.example.springboot.models;
+
+public class Employee {
+    private static int empid;
+    private String Name;
+    private String role;
+
+    public Employee(int empid, String name, String role) {
+        this.empid = empid;
+        this.Name = name;
+        this.role = role;
+    }
+
+    public Employee() {
+
+    }
+
+    public static int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
